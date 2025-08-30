f625f5bddf57833ebf1671d8e774efd72e2e2688
abujaher5 <abujaher5120@gmail.com>
2025-08-30 21:35:29 +0600

    create delete api formanage doctor page

--- index.js
+++ index.js
@@ -89,6 +89,12 @@ async function run() {
       const result = await doctorCollection.insertOne(item);
       res.send(result);
     });
+    app.delete("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
